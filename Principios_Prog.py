d8c60b5577a9acdaef6c2e07605f8d80c0f36eb7
vaquejano <vaquejano@gmail.com>
2022-04-16 12:15:44 -0300

    livro mercFinanceiro
    Este projeto estou desenvolvendo conforme vou avançando nos estudos deste livro.
    Acredito que as informações poderá ajudar muitas pessoas como eu no inicio com duvidas  de o que usar e como usar. espero que seja de grande ajuda

---
+++ Principios_Prog.py
@@ -0,0 +1,40 @@
+
+
+#Operacoes simples
+#funcoes pre pre-programadas
+print(1+1)#Soma
+print(3/2)#divisao
+print(4-10)#Subtracao
+#operacao de potenciacao:
+print(2**3)
+#resto divisao
+print(10 % 3)
+print("*******Armazenamento de variaves******")
+#armazenamento de dados em variaveis
+x=5
+y=10
+#para visializar o valor das variaveis ou do conjunto de dados
+#usa-se o comnado print()
+print("Mensagem de texto para verificar as variaveis")
+print("x=",x, "y=",y)#mensagem de texto para verificar as variaveis
+print(x)
+print(y)
+#Para ver o valor de duas variaveis ao mesmo tempo
+print("Para ver o valor de duas variaveis ao mesmo tempo")
+print(x,y)
+
+print("soma: ", x+y)
+print("subtracao: ", x-y)
+print("divisao: ", x/y)
+print("potenciacao: ", x**y)
+
+print("*******Importando biblioteca matematica******")
+#biblioteca
+import math as mt
+
+print("retorna e elevado a x: ",mt.exp(1))
+print("retorna o logaritimo de x na base 2: ",mt.log2(3))
+print("retorna o logaritimo de x na base 10: ",mt.log10(20))
+print("retorna o logaritimo natural de x (base e: ",mt.log(10))
+print("retorna a raiz quadradade x: ",mt.sqrt(16))
+print("retorna x elevado a y: ",mt.pow(2,3))
